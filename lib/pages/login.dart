ab4698ac235d015c39dafe77d1334f2fb9db38ca
julian <julian@skobic.eu>
2024-01-22 22:46:52 +0100

    changed app structure

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,27 +4,30 @@ import 'package:leaflearn/router/routes.dart';
 import 'package:leaflearn/services/loginservice.dart';
 import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage(this.from, {Key? key}) : super(key: key);
   final String? from;
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.read<LoginInfo>().login('user');
-            if(from !=null && from != '/home') {
-              context.go(from!);
-            } else {
-              context.go(HomeRoute(loggedIn: true).location);
-            }
-          },
-          child: Text('Login'),
-          )
-      ),
+          child: ElevatedButton(
+        onPressed: () {
+          context.read<LoginInfo>().login('user');
+          if (widget.from != null && widget.from != '/home') {
+            context.go(widget.from!);
+          } else {
+            context.go(HomeRoute(loggedIn: true).location);
+          }
+        },
+        child: Text('Login'),
+      )),
     );
   }
-
 }
